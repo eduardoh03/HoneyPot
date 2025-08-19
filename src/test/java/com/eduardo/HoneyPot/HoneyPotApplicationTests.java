f4fd801ab3441122f7065a8cbf4da23f901709d8
Eduardo Henrique <eduhenriquesrs@gmail.com>
2025-08-19 16:58:19 -0300

    first commit

---
+++ src/test/java/com/eduardo/HoneyPot/HoneyPotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eduardo.HoneyPot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HoneyPotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
